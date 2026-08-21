8d25988ba576197ede1a0a094e9cff08e79ea147
Jiemin-Z <Jiemin.zhao21@gmail.com>
2026-08-21 20:37:29 +0800

    include article1.md and about.md

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-fetch("about.txt")
+fetch("./article1.md")
 .then(response => response.text())
-.then(text => {
-    document.getElementById("about-text").innerText = Text;
+.then(markdown => {
+    document.getElementById("article1").innerHTML = marked.parse(markdown);
+});
+
+fetch("./about.md")
+.then(response => response.text())
+.then(markdown => {
+    document.getElementById("about").innerHTML = marked.parse(markdown);
 });
